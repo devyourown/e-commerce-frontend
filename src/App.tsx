b52d23a6bf43e6b62f9c45a23beb920c02fd2ed7
jhon3242 <rldotnr2@gmail.com>
2023-04-01 00:24:59 +0900

    feat(LoginPage) : 로그인 페이지 컴포넌트 작성 및 css 작성
    이벤트는 아직 작성하지 않았다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import LoginPage from './components/LoginPage';
+// typescript 로 회원가입 버튼과 아이디 찾기 버튼이 있는 회원가입 페이지를 만들어줘
 
 function App() {
   return (
-    <div>Hello</div>
+    <LoginPage />
   );
 }
 
